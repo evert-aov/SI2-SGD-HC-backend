abb10e4eb19ce005a5d1151933d7b1f418d68335
SCANDAL9 <duglasrivero2@gmail.com>
2026-04-06 15:31:29 -0400

    Autorizar usuarios por permisos específicos

--- src/main/java/com/sgd_hc/sgd_hc/module_users/controller/UserController.java
+++ src/main/java/com/sgd_hc/sgd_hc/module_users/controller/UserController.java
@@ -23,34 +23,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/api/module_users/users")
 @RequiredArgsConstructor
-@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class UserController {
+
     private final UserService userService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('USER_CREATE')")
     public ResponseEntity<UserResponseDto> userCreate(@RequestBody UserCreateDto entity) {
         return new ResponseEntity<>(userService.createUser(entity), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('USER_READ')")
     public ResponseEntity<Iterable<UserResponseDto>> getAllUser() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER_READ')")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER_UPDATE')")
     public ResponseEntity<UserResponseDto> userUpdate(@PathVariable Long id, @RequestBody UserUpdateDto entity) {
         return ResponseEntity.ok(userService.updateUser(id, entity));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER_DELETE')")
     public ResponseEntity<Void> userDelete(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
-
+    
 }
