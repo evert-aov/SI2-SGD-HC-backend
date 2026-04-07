6db04c46197932a178a416b97052acee4e4f5a4c
evert.aov <evert.aov@gmail.com>
2026-04-06 21:15:21 -0400

    feat: Add Swagger configuration for API documentation with JWT security

---
+++ src/main/java/com/sgd_hc/sgd_hc/security/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.sgd_hc.sgd_hc.security.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String securityScheme = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SGD HC API")
+                        .version("v1")
+                        .description("Documentacion de los endpoints de la API de SGD HC"))
+                .addSecurityItem(new SecurityRequirement().addList(securityScheme))
+                .components(new Components()
+                        .addSecuritySchemes(securityScheme,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
